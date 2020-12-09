2ecdeeafa00777e29530213c5ef7619c343e7e19
galla-okto <galla.okto1@gmail.com>
2020-12-09 16:23:08 +0200

    Добавляет модель данных User

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Text, Boolean, ForeignKey, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, Session
+
+engine = create_engine("sqlite:///my_post.db")
+Base = declarative_base(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), nullable=False)
+
+    posts = relationship("Post", back_populates="user")
+
+    def __str__(self):
+        return str(self.username)
+
+    def __repr__(self):
+        return str(self)
+
